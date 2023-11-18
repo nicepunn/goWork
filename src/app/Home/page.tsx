3cef4abe2953cff861bdd2c84159a45fe6538494
Punnawat Lohanuit <punnawat58000@gmail.com>
2023-11-18 19:39:28 +0700

    Fix: global.css-Background color

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -3,7 +3,7 @@ import ThumbnailCard from "@/component/ThumbnailCard";
 
 export default function Home() {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main >
         <ThumbnailCard/>
       </main>
     )
--- src/component/ThumbnailCard.tsx
+++ src/component/ThumbnailCard.tsx
@@ -2,8 +2,8 @@
 export default function ThumbnailCard() {
 
     return (
-        <div>
-            
+        <div className='w-[300px] h-[350px] bg-sky-600 items-center'>
+            asdf
         </div>
     )
 }
