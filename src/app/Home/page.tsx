1ceb79b6ca5d5c3497d626c4dd4c798b77fc41c8
Punnawat Lohanuit <punnawat58000@gmail.com>
2023-11-22 16:09:38 +0700

    Fix: api in home page

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -3,49 +3,49 @@
 import GoworkCatalog from "@/component/GoWorkCatalog";
 import getAllGoWork from "@/libs/getAllGoWork";
 import { useEffect, useState } from "react";
-const allGoWorkJsonRaw = {
-  "success": true,
-  "count": 3,
-  "pagination": {},
-  "data": [
-    {
-      "_id": "6559aa25e8addd217fbe8df4",
-      "name": "Samyan Co-op",
-      "operatingHours": "24 hours",
-      "address": "Samyan Mitrtown",
-      "province": "Bangkok",
-      "postalcode": "10330",
-      "tel": "+66 2 219 6999",
-      "picture": "https://drive.google.com/uc?id=1JinSybZyq4eMEJTUVewuh9X87DYznuc3",
-      "__v": 0,
-      "id": "6559aa25e8addd217fbe8df4"
-    },
-    {
-      "_id": "6559aa83e8addd217fbe8df7",
-      "name": "Chula Libaray",
-      "operatingHours": "24 hours",
-      "address": "Chulalongkorn University",
-      "province": "Bangkok",
-      "postalcode": "10330",
-      "tel": "+66 2-218-2929",
-      "picture": "https://drive.google.com/uc?id=1Otx0rTEVbXahcxcXZTvoM9QnlRJmubOP",
-      "__v": 0,
-      "id": "6559aa83e8addd217fbe8df7"
-    },
-    {
-      "_id": "6559aa83e8addd217fbe8dg0",
-      "name": "Chula Engineering Libaray",
-      "operatingHours": "24 hours",
-      "address": "Faculty of Engineering Chulalongkorn University",
-      "province": "Bangkok",
-      "postalcode": "10330",
-      "tel": "+66 2-218-1234",
-      "picture": "https://drive.google.com/uc?id=1PJC6JwBgjMkK8RgMGi4al13feVCE6l3c",
-      "__v": 0,
-      "id": "6559aa83e8addd217fbe8dg0"
-    }
-  ]
-}
+// const allGoWorkJsonRaw = {
+//   "success": true,
+//   "count": 3,
+//   "pagination": {},
+//   "data": [
+//     {
+//       "_id": "6559aa25e8addd217fbe8df4",
+//       "name": "Samyan Co-op",
+//       "operatingHours": "24 hours",
+//       "address": "Samyan Mitrtown",
+//       "province": "Bangkok",
+//       "postalcode": "10330",
+//       "tel": "+66 2 219 6999",
+//       "picture": "https://drive.google.com/uc?id=1JinSybZyq4eMEJTUVewuh9X87DYznuc3",
+//       "__v": 0,
+//       "id": "6559aa25e8addd217fbe8df4"
+//     },
+//     {
+//       "_id": "6559aa83e8addd217fbe8df7",
+//       "name": "Chula Libaray",
+//       "operatingHours": "24 hours",
+//       "address": "Chulalongkorn University",
+//       "province": "Bangkok",
+//       "postalcode": "10330",
+//       "tel": "+66 2-218-2929",
+//       "picture": "https://drive.google.com/uc?id=1Otx0rTEVbXahcxcXZTvoM9QnlRJmubOP",
+//       "__v": 0,
+//       "id": "6559aa83e8addd217fbe8df7"
+//     },
+//     {
+//       "_id": "6559aa83e8addd217fbe8dg0",
+//       "name": "Chula Engineering Libaray",
+//       "operatingHours": "24 hours",
+//       "address": "Faculty of Engineering Chulalongkorn University",
+//       "province": "Bangkok",
+//       "postalcode": "10330",
+//       "tel": "+66 2-218-1234",
+//       "picture": "https://drive.google.com/uc?id=1PJC6JwBgjMkK8RgMGi4al13feVCE6l3c",
+//       "__v": 0,
+//       "id": "6559aa83e8addd217fbe8dg0"
+//     }
+//   ]
+// }
 export default function Home() {
 
   const [allGoWorkResponse, setAllGoWorkResponse] = useState(null);
@@ -54,10 +54,11 @@ export default function Home() {
     const fetchData = async () => {
       const allGoWork = await getAllGoWork()
       setAllGoWorkResponse(allGoWork)
+      console.log(allGoWork)
     }
+    fetchData()
   }, [])
 
-    
   return (
     <main >
       <GoworkCatalog allGoWorkJson={allGoWorkResponse}/>
